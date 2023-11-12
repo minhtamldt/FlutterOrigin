1d8d28984da4422ce6697ee94071f189538ca0ca
TamTM <minhtamldt@gmail.com>
2023-11-13 00:20:52 +0700

    save code

--- flutter_origin/lib/app.dart
+++ flutter_origin/lib/app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_origin/presentation/counter/counter_page.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class OriginApp extends StatelessWidget {
   const OriginApp({super.key});
@@ -8,6 +8,8 @@ class OriginApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
@@ -28,7 +30,61 @@ class OriginApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const CounterPage(title: 'Flutter Demo Home Page'),
+      // home: const CounterPage(title: 'Flutter Demo Home Page'),
+      home: _buildHome(context),
     );
+  }
+
+  _buildHome(BuildContext context) {
+    return Builder(builder: (context) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text(AppLocalizations.of(context)!.hello('Flutter')),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              // Add the following code
+              Localizations.override(
+                context: context,
+                locale: const Locale('vi'),
+                // Using a Builder to get the correct BuildContext.
+                // Alternatively, you can create a new widget and Localizations.override
+                // will pass the updated BuildContext to the new widget.
+                child: Builder(
+                  builder: (context) {
+                    // A toy example for an internationalized Material widget.
+                    return Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          const Text(
+                              "Lấy Text `Hello World` từ AppLocalizations"),
+                          Text(AppLocalizations.of(context)!.helloWorld,
+                              style: const TextStyle(color: Colors.red)),
+                          const Text("Dùng Placeholder: Hello {userName}"),
+                          Text(
+                              AppLocalizations.of(context)!
+                                  .hello("Đây là user name"),
+                              style: const TextStyle(color: Colors.red)),
+                          const Text("Dùng Plurals"),
+                          Text(AppLocalizations.of(context)!.nWombats(50),
+                              style: const TextStyle(color: Colors.red)),
+                          const Text("Dùng Selects"),
+                          Text(AppLocalizations.of(context)!.pronoun('male'),
+                              style: const TextStyle(color: Colors.red)),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    });
   }
 }
