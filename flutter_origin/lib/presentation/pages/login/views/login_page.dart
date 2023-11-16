a5481c1ecf4e3caa142fb97edf8b2378bcd15237
TamTM <minhtamldt@gmail.com>
2023-11-16 09:42:37 +0700

    clean code

--- flutter_origin/lib/presentation/pages/login/views/login_page.dart
+++ flutter_origin/lib/presentation/pages/login/views/login_page.dart
@@ -1,7 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_origin/config/multi_language/multi_languages.dart';
 import 'package:flutter_origin/core/base/widgets/page_widget.dart';
 import 'package:flutter_origin/presentation/pages/login/bloc/login_page_bloc.dart';
 import 'package:flutter_origin/presentation/pages/login/bloc/login_page_state.dart';
@@ -23,32 +22,35 @@ class _LoginPageState extends State<LoginPage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-           _buidLogo(),
-           _buildEmailField(),
+            _buidLogo(),
+            _buildEmailField(),
           ],
         ),
       );
     }));
   }
-  
+
   _buidLogo() {
     return Column(
       children: <Widget>[
         Image.asset(
           'assets/images/news_logo.png',
         ),
-          const SizedBox(
+        const SizedBox(
           height: 20,
         ),
-        Text(MultiLanguages.of(context).translate("login"), style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),)
+        const Text(
+          "login",
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        )
       ],
     );
   }
-  
+
   _buildEmailField() {
     return Column(
       children: <Widget>[
-        Text(MultiLanguages.of(context).translate("login")),
+        const Text("login"),
         const SizedBox(
           height: 10,
         ),
@@ -57,7 +59,8 @@ class _LoginPageState extends State<LoginPage> {
             hintText: 'Email',
             border: OutlineInputBorder(),
           ),
-        ),],
+        ),
+      ],
     );
   }
 }
